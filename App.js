2487eb9212d31887b46d86a17d4e591bc9b5492a
Allejandropg <alle401@gmail.com>
2020-06-06 17:43:02 -0300

    inital commit

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Index() {
+  return <View style={{flex:1,alignItems:"center",justifyContent:"center"}}><Text>Squad 3</Text></View>;
+}
